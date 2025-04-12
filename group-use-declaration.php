7544dfc251859c5b913159649d12e11e2e044c81
alyouma <alyoumaakmal4@gmail.com>
2025-04-12 17:35:46 +0700

    group use declaration

--- group-use-declaration.php
+++ group-use-declaration.php
@@ -3,10 +3,9 @@
 require_once "data/Conflict.php";
 require_once "data/Helper.php";
 
-use Data\One\Conflict as Conflict1;
-use Data\Two\Conflict as Conflict2;
-use function Helper\HelpMe as help;
-use const Helper\APPLICATION as APP;
+use Data\One\{Conflict as Conflict1, Conflict as Conflict2};
+use function Helper\{HelpMe as help};
+use const Helper\{APPLICATION as APP};
 
 $conflict1 = new Conflict1();
 $conflict2 = new Conflict2();
