d1e7e5fb68a4d2b25d35eaf7d1bcfd47ab6cef1b
alyouma <alyoumaakmal4@gmail.com>
2025-04-12 15:47:51 +0700

    manipulasi properties

--- object.php
+++ object.php
@@ -4,5 +4,7 @@ require_once "data/Person.php";
 
 $person = new Person();
 
-var_dump($person);
+echo " Name : {$person->name}" . PHP_EOL;
+echo " Addres : {$person->addres}" . PHP_EOL;
+echo " Country : {$person->country}" . PHP_EOL;
 ?>
