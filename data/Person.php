39629a2c1ae0e4ba9b171c0011d92dbf5083e2a3
alyouma <alyoumaakmal4@gmail.com>
2025-04-13 17:38:56 +0700

    abstract function

--- data/Person.php
+++ data/Person.php
@@ -9,13 +9,25 @@ trait HasName
     public string $name;
 }
 
+trait CanRun
+{
+    public abstract function run(): void;
+}
 class Person
 {
-    use SayGoodBye, HasName;
+    use SayGoodBye, HasName, CanRun;
+
+    // abstract function
+    public function run(): void
+    {
+        echo " Person {$this->name} is running" . PHP_EOL;
+    }
 }
 
 
 $person = new Person();
 $person->goodBye("ga");
 $person->name = "akmal";
-echo "Name is : {$person->name}";
+echo "Name is : {$person->name}" . PHP_EOL;
+// abstract function
+$person->run();
