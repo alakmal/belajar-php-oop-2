cd309d5eb50344da68742b41a4d6c49a4ca577e8
alyouma <alyoumaakmal4@gmail.com>
2025-04-13 11:29:52 +0700

    function argument polymorphisme

--- polymorphisme.php
+++ polymorphisme.php
@@ -15,3 +15,13 @@ $company = new Company();
 $company->programmer = new Programmer("Eko");
 $company->programmer = new BackEndProgrammer('Eko');
 $company->programmer = new FrontEndProgrammer("eko");
+
+// functio argument polymorphisme
+function sayHello(Programmer $programmer)
+{
+    echo "Hello {$programmer->name}" . PHP_EOL;
+}
+
+sayHello(new Programmer("Eko"));
+sayHello(new BackEndProgrammer("Budi"));
+sayHello(new BackEndProgrammer("Joko"));
