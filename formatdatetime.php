70c2b1df6eb60f45e6a7ebd9c624e8aee40cd3df
alyouma <alyoumaakmal4@gmail.com>
2025-04-14 12:18:02 +0700

    parsedatetime

--- formatdatetime.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-
-$date = new DateTime();
-$date->setTimezone(new DateTimeZone("Asia/Jakarta"));
-
-echo $date->format("Y-m-d H:i:s");
---
+++ parsedatetime.php
@@ -0,0 +1,6 @@
+<?php
+
+
+$date = DateTime::createFromFormat("Y-m-d H:i:s", "1998-12-29 10:10:10");
+
+var_dump($date);
