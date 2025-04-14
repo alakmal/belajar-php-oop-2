a24883cad33a2315b549af5806904284e42ee43a
alyouma <alyoumaakmal4@gmail.com>
2025-04-14 09:08:09 +0700

    anonymous class

---
+++ anonymous-class.php
@@ -0,0 +1,17 @@
+<?php
+
+interface HelloWorld
+{
+
+    function sayHello(): void;
+}
+
+
+$helloWorld = new class  implements HelloWorld {
+    function sayHello(): void
+    {
+        echo "Hello Anonymous Class" . PHP_EOL;
+    }
+};
+
+$helloWorld->sayHello();
