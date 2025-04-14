7d639a02dc140d8880e1f5d18f1643abfeb6dcef
alyouma <alyoumaakmal4@gmail.com>
2025-04-14 09:20:29 +0700

    static function

--- data/MathHelper.php
+++ data/MathHelper.php
@@ -7,7 +7,19 @@ namespace Data;
 class MathHelper
 {
     static public string $name = "Math Helper";
+
+    static public function sum(int ...$numbers): int
+    {
+        $total = 0;
+        foreach ($numbers as $number) {
+            $total += $number;
+        }
+
+        return $total;
+    }
 }
 
 
 echo MathHelper::$name . PHP_EOL;
+$total = MathHelper::sum(10, 10, 10, 230, 30);
+echo "Total $total" . PHP_EOL;
